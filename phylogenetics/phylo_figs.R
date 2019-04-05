5d82a93188eac2b9839c89ca8eccac742da13d3d
ajrominger <ajrominger@gmail.com>
2019-04-05 16:52:55 -0600

    working on lining up sequences with phylo

--- phylogenetics/phylo_figs.R
+++ phylogenetics/phylo_figs.R
@@ -8,14 +8,17 @@ tre <- rphylo(5, 1, 0.9)
 
 set.seed(123)
 rootS <- sample(c('a', 'c', 'g', 't'), 10, replace = TRUE)
-rootS <- rep(c('a', 'c', 'g', 't'), 3)[1:10]
-Q <- matrix(c(0.0, 0.2, 0.2, 1.0,
-              0.2, 0.0, 1.0, 0.2, 
-              0.2, 1.0, 0.0, 0.2, 
-              1.0, 0.2, 0.2, 0.0), 
+Q <- matrix(c(0.0, 0.2, 1.0, 0.2,
+              0.2, 0.0, 0.0, 1.0, 
+              1.0, 0.2, 0.2, 0.2, 
+              0.2, 1.0, 0.2, 0.0), 
             byrow = TRUE, nrow = 4)
 diag(Q) <- - rowSums(Q)
-
+set.seed(12)
 s <- simSeq(tre, l = length(rootS), rootseq = rootS, type = 'DNA', rate = 0.1, Q = Q)
-rbind(rootS, as.character(s))
+s <- toupper(as.character(s))
 
+plot(tre, x.lim = c(0, 10), show.tip.label = FALSE)
+for(i in 1:ncol(s)) {
+    tiplabels(s[, i], frame = 'none', adj = 4 - i)
+}
