e53c074e6f6b9207b9d8d6ac0dda3ac793285860
ajrominger <ajrominger@gmail.com>
2019-04-19 16:35:09 -0600

    added example phylo problem figs

--- phylogenetics/phylo_figs.R
+++ phylogenetics/phylo_figs.R
@@ -38,6 +38,11 @@ for(i in 1:nrow(s)) {
 
 dev.off()
 
+# unrooted phylo
+pdf('phylogenetics/fig_phyloUnroot.pdf', width = 4, height = 4)
+par(mar = rep(0, 4))
+plot(tre, type = 'unrooted', show.tip.label = FALSE, edge.width = 3)
+dev.off()
 
 
 # likelihood maximization ----
@@ -58,4 +63,34 @@ persp(z,
       theta = -40, phi = 30, expand = 3, scale = FALSE,
       shade = NA, col = colz[zFacetRange], border = 'grey',
       zlim = c(0.01, 0.06), box=FALSE)
+dev.off()
+
+# human, cat, squirrel, bird phylo ----
+mb <- read.tree(text = '(bird, (cat, (squirrel, human)));')
+
+pdf('phylogenetics/fig_phylo_mamBird.pdf', width = 4, height = 4)
+par(mar = rep(0, 4))
+plot(mb, edge.width = 3)
+dev.off()
+
+# non-chronogram
+mb$edge.length <- c(6, 8.25, 4, 0.25, 0.75, 1.75)
+pdf('phylogenetics/fig_phylo_mamBird_noChrono.pdf', width = 4, height = 4)
+par(mar = rep(0, 4))
+plot(mb, edge.width = 3)
+dev.off()
+
+
+# chronogram
+mbCalib <- read.tree('phylogenetics/mb.nwk')
+
+pdf('phylogenetics/fig_phylo_mamBird_chrono.pdf', width = 4, height = 5)
+layout(matrix(1:2, nrow = 2), heights = c(4, 2))
+par(mar = c(0, 1, 0, 0))
+plot(mbCalib, show.tip.label = FALSE, edge.width = 3)
+xlim <- par('usr')[2:1]
+par(mar = c(5, 1, 0, 0), xpd = NA)
+plot(1, type = 'n', xlim = xlim, xaxs = 'i', axes = FALSE, xlab = '')
+socorro::paleoAxis(1)
+mtext('Millions of years ago', side = 1, line = 4)
 dev.off()
